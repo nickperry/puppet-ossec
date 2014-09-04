b6576a1e5c15daad9ae5ec539344bc6639ce83da
Justice London <jlondon@syrussystems.com>
2014-09-04 10:54:20 -0700

    More ossec bugfixes

--- manifests/server.pp
+++ manifests/server.pp
@@ -13,7 +13,7 @@ class ossec::server (
   # install package
   case $lsbdistid {
     /(Ubuntu|ubuntu|Debian|debian)/ : {
-      package { "ossec-hids-server":
+      package { $ossec::common::hidsserverpackage:
         ensure  => installed,
         require => Apt::Ppa['ppa:nicolas-zin/ossec-ubuntu'],
       }
@@ -50,14 +50,13 @@ class ossec::server (
     mode    => 0440,
     require => Package[$ossec::common::hidsserverpackage],
     notify  => Service[$ossec::common::hidsserverservice]
-  }
+  } 
   concat::fragment { "ossec.conf_10" :
     target  => '/var/ossec/etc/ossec.conf',
     content => template("ossec/10_ossec.conf.erb"),
     order   => 10,
     notify  => Service[$ossec::common::hidsserverservice]
   }
-
   concat::fragment { "ossec.conf_90" :
     target  => '/var/ossec/etc/ossec.conf',
     content => template("ossec/90_ossec.conf.erb"),
@@ -65,20 +64,19 @@ class ossec::server (
     notify  => Service[$ossec::common::hidsserverservice]
   }
 
-  include concat::setup
   concat { "/var/ossec/etc/client.keys":
     owner   => "root",
     group   => "ossec",
     mode    => "640",
-    notify  => Service[$ossec::common::hidsserverservice]
+    notify  => Service[$ossec::common::hidsserverservice],
+    require => Package[$ossec::common::hidsserverpackage],
   }
-  Ossec::AgentKey<<| |>>
-
   concat::fragment { "var_ossec_etc_client.keys_end" :
     target  => "/var/ossec/etc/client.keys",
     order   => 99,
     content => "\n",
     notify  => Service[$ossec::common::hidsserverservice]
   }
+  Ossec::AgentKey<<| |>>
 
 }
