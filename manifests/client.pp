918062237ef5182ba518aaf3fb6e3cc8df11f825
WBasson <willem@willembasson.co.za>
2015-03-18 11:51:56 +0200

    Update client.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -9,7 +9,7 @@ class ossec::client(
     'Debian' : {
       package { $ossec::common::hidsagentpackage:
         ensure  => installed,
-        require => Apt::Source["alienvault"],
+        require => Apt::Source['alienvault'],
       }
     }
     'RedHat' : {
