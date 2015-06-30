a0563a13cb6f40709e128808d4bdc4279b201f8a
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2015-06-30 14:05:13 +0100

    Use agent_name to distinguish the agent keys so there are not issues with IP addresses being reused

--- manifests/agentkey.pp
+++ manifests/agentkey.pp
@@ -10,7 +10,7 @@ define ossec::agentkey(
   $agentKey1 = ossec_md5("${agent_id} ${agent_seed}")
   $agentKey2 = ossec_md5("${agent_name} ${agent_ip_address} ${agent_seed}")
 
-  concat::fragment { "var_ossec_etc_client.keys_${agent_ip_address}_part":
+  concat::fragment { "var_ossec_etc_client.keys_${agent_name}_part":
     target  => '/var/ossec/etc/client.keys',
     order   => $agent_id,
     content => "${agent_id} ${agent_name} ${agent_ip_address} ${agentKey1}${agentKey2}\n",
